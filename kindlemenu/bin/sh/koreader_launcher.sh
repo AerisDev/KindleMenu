6d7e4a9a1826d71fb1beee6e5ad04d8af9bb4c81
Aeris <aerisdeveloper@gmail.com>
2013-11-02 14:37:33 +0100

    Fix and add
    Fixed koreader launcher and added info tool

--- kindlemenu/bin/sh/koreader_launcher.sh
+++ kindlemenu/bin/sh/koreader_launcher.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
+sleep 12
 /mnt/us/koreader/koreader.sh /mnt/us/documents
 # wait the script to finish, i.e. until koreader closes
 wait
 # sleep a while until chrome bar is displayed again
-sleep 8
+sleep 4
 /mnt/us/extensions/kindlemenu/bin/sh/batterylevel.sh
 /mnt/us/extensions/kindlemenu/bin/shortcut.sh
 
