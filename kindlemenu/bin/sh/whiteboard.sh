d2ba94deeba7a2b187d1896f2c357f18243dac34
Aeris <aerisdeveloper@gmail.com>
2013-11-08 19:22:25 +0100

    update06
    0.6*
    - NEW APP ADDED: Kindle Gallery 1.5
    - improvements in app loading scripts
    - semplified whiteboard apps to avoid some possible bugs
    - slightly better graphic, with shadow boxes
    - minor improvemets

--- kindlemenu/bin/sh/whiteboard.sh
+++ kindlemenu/bin/sh/whiteboard.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
-init_orientation=`/usr/bin/lipc-get-prop com.lab126.winmgr orientationLock`
-/usr/bin/lipc-set-prop com.lab126.winmgr orientationLock U
 SSSTATE=`lipc-get-prop com.lab126.powerd preventScreenSaver`
 lipc-set-prop com.lab126.powerd preventScreenSaver 0
-/mnt/us/extensions/kindlemenu/bin/sh/keepalive.sh &
 /usr/bin/xtestlab126 &
-( dbus-monitor "interface='com.lab126.powerd',member='goingToScreenSaver'" --system;  killall keepalive.sh; killall xtestlab126; lipc-set-prop com.lab126.powerd powerButton 1; lipc-set-prop com.lab126.powerd preventScreenSaver "$SSSTATE" ) &
+( dbus-monitor "interface='com.lab126.powerd',member='goingToScreenSaver'" --system; sleep 2;  killall xtestlab126; lipc-set-prop com.lab126.powerd powerButton 1; lipc-set-prop com.lab126.powerd preventScreenSaver "$SSSTATE" ) &
 usleep 50000
 killall -INT dbus-monitor
-/usr/bin/lipc-set-prop com.lab126.winmgr orientationLock ${init_orientation}
 
